dd7b92589966d105dc525367ad6cbdcc45148f6c
eddi <h.amiltone.diel@gmail.com>
2021-05-13 23:59:59 -0500

    Agregando Home

--- src/core/layout/components/Navbar.js
+++ src/core/layout/components/Navbar.js
@@ -1,23 +1,30 @@
 import React, {Component}from 'react';
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+// import {Logo} from '../../../logo.svg';
+import gamer from '../../../assets/img/gamer.png';
 
 class Navbar extends Component {
     render() { 
         return ( 
-            <div  className="navbar">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/home">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Dashboard</Link>
-                    </li>
-                </ul>
-                <hr />
-            </div>
+            <nav  className="header_">
+                <div className="selection_ header-icons-container">
+                    <div className="icons">
+                        <Link to="/"><span className="flaticon-001-facebook"></span></Link>
+                        <Link to="/"><span className="flaticon-002-twitter"></span></Link>
+                        <Link to="/"><span className="flaticon-011-instagram"></span></Link>
+                        <Link to="/"><span className="flaticon-010-linkedin"></span></Link>
+                        <Link to="/"><span className="flaticon-008-youtube"></span></Link>
+                    </div>
+                </div>
+                <div className="nav_">
+                    <div className="selection_ nav-logo-container">
+                       <Link to="/"> <img src={gamer} alt="Logo de mi blog"/> Mi blog</Link>
+                    </div>
+                    <div className="selection_ profile-link">
+                        <Link to="">Sobre mi</Link>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
 import React, { Component} from 'react';
+// import './style/Home.css';
+import {Link} from 'react-router-dom';
+import Particles from 'react-particles-js';
+
+
+
 
 export default class Home extends Component {
     render() { 
+
+        const styles = {
+            root: {
+                fontFamily: "sans-serif",
+                textAlign: "center",
+                height: "100%",
+                background: "#222",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }
+        };
+        
+
         return ( 
-            <h3>Comport NotFount</h3>
+            <div style={styles.root}>
+                <Particles />
+            </div>
         );
     }
 }
