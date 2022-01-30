802bba9bf7eab9ad91cc517f7ff5acb7a8f1f476
Ediel96 <johndoe@example.com>
2022-01-30 17:05:57 -0500

    ejemplo con mockap

--- src/pages/NotFount.js
+++
@@ -1,9 +0,0 @@
-import React, { Component} from 'react';
-
-export default class NotFount extends Component {
-    render() { 
-        return ( 
-            <h3>Comport NotFount</h3>
-        );
-    }
-}
