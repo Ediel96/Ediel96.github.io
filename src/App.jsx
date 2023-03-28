b480e4d6309bbbf9089c1b0b7b3313e1a57527c9
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2023-03-27 21:48:03 -0500

    example eddie

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import { AppTheme } from './theme/AppTheme'
-import { CssBaseline } from "@mui/material"
-import { AppRouter } from './router/AppRouter'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <AppTheme>
-      <CssBaseline/>
-      <AppRouter/>
-    </AppTheme>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   )
 }
+
+export default App
